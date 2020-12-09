66c67ca8a942c260fdc976de9225cdc7cb12b0a0
Anders Mörtsell <anders.mortsell@sogeti.se>
2020-12-09 22:00:22 +0100

    Finished day 9

---
+++ AdventOfCode - Day9.1.ps1
@@ -0,0 +1,31 @@
+﻿$inputs = @()        
+
+Get-Content -Path '.\numbers day 9.txt' | ForEach-OBject {
+  $inputs += [Int64]::Parse($_)
+}
+
+for ($i = 25; $i -lt $inputs.Count; $i++) {
+  $valid = $false
+  for ($j = $i - 25; $j -lt $i; $j++) {
+    for ($k = $i - 25; $k -lt $i; $k++) {
+      if ($j -eq $k) {
+        continue
+      }
+      if ($inputs[$j] + $inputs[$k] -eq $inputs[$i]) {
+        $valid = $true
+        break
+      }
+    }
+    if ($valid) {
+      break
+    }
+  }
+
+  if (!$valid) {
+    $inputs[$i]
+    break;
+  }
+}
+
+
+
---
+++ AdventOfCode - Day9.2.ps1
@@ -0,0 +1,40 @@
+﻿$inputs = New-Object System.Collections.ArrayList
+
+Get-Content -Path '.\numbers day 9.txt' | ForEach-OBject {
+  $inputs.Add([Int64]::Parse($_))
+}
+
+function checkSum ($arr, $i, $target, $tmp) {
+    $sum = 0
+    while ($sum -le $target -and $i -lt $arr.Count) {
+        $index = $tmp.Add($arr[$i])
+        $sum += $arr[$i]
+        $i++
+        if ($sum -eq $target) {
+            return $true
+        }
+    }
+
+    return $false
+}
+
+$target = 177777905
+
+$res = New-Object System.Collections.ArrayList
+for ($i = 0; $i -lt $inputs.Count - 1; $i++) {
+    $tmp = New-Object System.Collections.ArrayList
+    $test = checkSum $inputs $i $target $tmp
+    if ($test) {
+        if ($tmp.Count -gt 1) {
+            $res.Add($tmp)
+        }
+    }
+}
+
+if ($res.Count -eq 0) {
+  Write-Host "No combinations found"
+} else {
+    $res[0].Sort()
+    $res[0][0] + $res[0][$res[0].Count - 1]
+}
+
